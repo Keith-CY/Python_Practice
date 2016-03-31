5fa0e85a1012e4ef28618375ec0e3582e22a39ea
Chen Yu <keithwhisper@gmail.com>
2016-03-31 17:32:23 +0800

    Number_Printer
    Enter a Number and Print in Large Mode

---
+++ Number_Printer.py
@@ -0,0 +1,20 @@
+zero = ["*****","*   *","*   *","*   *","*****"]
+one = ["  *  ","  *  ","  *  ","  *  ","  *  "]
+two = ["*****","    *","*****","*    ","*****"]
+three = ["*****","    *","*****","    *","*****"]
+four = ["*   *","*   *","*****","    *","    *"]
+five = ["*****","*    ","*****","    *","*****"]
+six = ["*****","*    ","*****","*   *","*****"]
+seven = ["*****","    *","    *","    *","    *"]
+eight = ["*****","*   *","*****","*   *","*****"]
+nine = ["*****","*   *","*****","    *","*****"]
+
+num = input("enter a number: ")
+digits = [zero, one, two, three, four, five, six, seven, eight, nine]
+length = len(num)
+for i in range(5):
+	line = ''
+	for digit in num:
+		line += digits[int(digit)][i]
+		line += '  '
+	print(line)
